60fde50bb2d2b95772726cdebc43bd69e6aabcf6
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-12 18:18:31 +0530

    Employe Model And Employe repository is created successfully

---
+++ src/main/java/com/Aj/Spring/Model/Employe.java
@@ -0,0 +1,24 @@
+package com.Aj.Spring.Model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+    @Column(name = "frist_name")
+    private String name;
+    @Column(name = "last_name")
+    private String lastname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "mobail")
+    private long number;
+
+
+}
---
+++ src/main/java/com/Aj/Spring/Repository/EmployeRepo.java
@@ -0,0 +1,4 @@
+package com.Aj.Spring.Repository;
+
+public class EmployeRepo {
+}
